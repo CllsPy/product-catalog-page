8356c4cd27ec7fc897ebcf3e36957f84733a84f3
Cllpsy <ifal.edu.2017@gmail.com>
2026-07-16 21:03:49 -0300

    green project

--- backend/src/config/upstash.js
+++
@@ -1,9 +0,0 @@
-import { Redis } from '@upstash/redis'
-
-const redis = new Redis({
-  url: 'https://verified-mosquito-152595.upstash.io',
-  token: 'gQAAAAAAAlQTAAIgcDJiMDQxYTc0YzViY2I0ZjExYmViZTVkMDE0NTI5OGNmYw',
-})
-
-await redis.set("foo", "bar");
-await redis.get("foo");
