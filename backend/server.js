7bbd6998dbc81dd93b5549280d6c803febe52378
Cllpsy <ifal.edu.2017@gmail.com>
2026-07-23 23:05:20 -0300

    feat: status code

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,10 @@ import express from 'express';
 
 const app = express();
 
+app.get('/api/notes', (req, res) => {
+  res.status(200).send("Your Research Your Notes!")
+})
+
 app.listen(3000, () => {
   console.log(`Server is running on port 3000`);
 });
