726292c2336f67490944911af3d196824cdb6b06
Cllpsy <ifal.edu.2017@gmail.com>
2026-07-27 21:51:58 -0300

    feat: nodemon + routes

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,19 @@ import express from 'express';
 const app = express();
 
 app.get('/api/notes', (req, res) => {
-  res.status(200).send("Your Research Your Notes!")
+  res.status(200).send("Your Research Your 20 NotessssS!!")
+})
+
+app.post('/api/create', (req, res) => {
+  res.status(201).json({message:"You created a new note!"})
+})
+
+app.put('/api/notes/:id', (req, res) => {
+  res.status(201).json({message:"You updated a note!"})
+})
+
+app.delete('/api/notes/:id', (req, res) => {
+  res.status(201).json({message:"You deleted a note!"})
 })
 
 app.listen(3000, () => {
