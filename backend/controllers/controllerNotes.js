e41341b5f19925be2aa1e5bea14792e97f254d1e
Cllpsy <ifal.edu.2017@gmail.com>
2026-08-10 20:37:04 -0300

    feat: controllers

---
+++ backend/controllers/controllerNotes.js
@@ -0,0 +1,15 @@
+export const getAllNotes = (req, res) => {
+    res.status(200).json({"message": "You just fetched the notes!"})
+}
+
+export const createNote = (req, res) => {
+    res.status(201).json({"message": "Note created sucessfully!"})
+}
+
+export const updateNote = (req, res) => {
+    res.status(200).json({"message": "Note updated sucessfully"})
+}
+
+export const deleteNote = (req, res) => {
+    res.status(200).json({"message": "Note deleted sucessfully"})
+}
