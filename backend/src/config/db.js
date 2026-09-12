fe903e974a0a9bc4fdb4f0ab6039bd0cb6ef44c3
Cllpsy <ifal.edu.2017@gmail.com>
2026-09-12 17:33:53 -0300

    feat: connected to mongodb

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -5,7 +5,7 @@ import mongoose from "mongoose";
 export const connectDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI)
-        console.log("Connected")
+        console.log("Connected To DB")
     } catch (error) {
         console.error("Error connecting")
         process.exit(1)
