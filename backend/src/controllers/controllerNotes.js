bd0301395dd9e37239ebd6d4927083e3c26975ed
Cllpsy <ifal.edu.2017@gmail.com>
2026-09-14 19:14:05 -0300

    fix: gellAllNotes method

--- backend/src/controllers/controllerNotes.js
+++ backend/src/controllers/controllerNotes.js
@@ -3,7 +3,7 @@ import Note from "../models/Note.js"
 
 export const getAllNotes = async (req, res) => {
     try {
-        const notes = await Note.findx();
+        const notes = await Note.find();
         res.status(200).json(notes);
 
     } catch (error) {
